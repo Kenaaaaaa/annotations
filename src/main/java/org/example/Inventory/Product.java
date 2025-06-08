3a0fdbc4e09a59808fda3ada0ad7fdbcfe1c03ba
kenaaaaaaa <kenatirana@yahoo.com>
2025-06-08 10:34:54 +0200

    Java annotations

---
+++ src/main/java/org/example/Inventory/Product.java
@@ -0,0 +1,26 @@
+package org.example.Inventory;
+/*do shkojme tek pom per te shkruajtur Dependency , e lombok
+<dependency>
+    <groupId>org.projectlombok</groupId>
+    <artifactId>lombok</artifactId>
+    <version>1.18.38</version>
+</dependency>
+lombok, ben gjenerimin e annottation
+* */
+
+
+
+import lombok.*;
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Product {
+
+    private String name;
+    private double price;
+
+
+}
