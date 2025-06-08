03fba40e75edf0aee25135bd5e6ac300fa48992b
kenaaaaaaa <kenatirana@yahoo.com>
2025-06-08 13:00:23 +0200

    Enum

---
+++ src/main/java/org/example/Author/Book.java
@@ -0,0 +1,18 @@
+package org.example.Author;
+enum Genre {
+    FANTASY,
+    SCIENCE_FICTION,
+    ROMANCE,
+    THRILLER,
+    NON_FICTION,
+    OTHER
+}
+
+public class Book {
+    private String title;
+    private double price;
+    private int yearOfPublication;
+    private String author;
+    private String genre;
+
+}
