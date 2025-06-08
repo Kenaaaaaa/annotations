a86a37d5bb9d070afd5609e6cd78ebc127c55eb7
kenaaaaaaa <kenatirana@yahoo.com>
2025-06-08 13:07:28 +0200

    lombok

---
+++ src/main/java/org/example/Author/BookService.java
@@ -0,0 +1,16 @@
+package org.example.Author;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+
+public class BookService {
+
+}
